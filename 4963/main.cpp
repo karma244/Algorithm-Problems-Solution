d7f6c77a607e0553ed4db5eada8b4c69473db4d7
karma244 <monkeyman7763@gmail.com>
2025-01-24 18:32:39 +0900

    Add: dfs

---
+++ 4963/main.cpp
@@ -0,0 +1,51 @@
+#include <bits/stdc++.h>
+#define TxtIO   freopen("output","w",stdout);
+using namespace std;
+
+int graph[51][51];
+int vis[51][51];
+int w, h; 
+int dx[8] = { 0, 0, 1, -1, -1, -1, 1, 1};
+int dy[8] = { -1, 1, 0, 0 , 1, -1, -1, 1};
+
+void dfs(int x, int y) {
+    for (int i = 0; i < 8; i++) {
+        int nx = x + dx[i];
+        int ny = y + dy[i];
+
+        if (nx >= 0 && ny >= 0 && nx < w && ny < h) {
+            if (!vis[nx][ny] && graph[nx][ny] == 1) {
+                vis[nx][ny] = 1;
+                dfs(nx, ny);
+            }
+        }
+    }
+}
+
+int main() {
+    TxtIO;
+
+    ios_base::sync_with_stdio(true); cin.tie(0);
+    while(1) {
+        cin >> w >> h;
+        if (w == 0 && h == 0) break;
+        for (int i = 0; i < h; i++) 
+            for (int j = 0; j < w; j++) 
+                cin >> graph[i][j];
+
+        int ret = 0;
+        for (int i = 0; i < w; i++) {
+            for (int j = 0; j < h; j++) {
+                if (!vis[i][j] && graph[i][j] == 1) { 
+                    vis[i][j] = 1;
+                    dfs(i, j);
+                    ret++;
+                }
+            }
+        }
+        cout << ret << '\n';
+        memset(vis, 0, sizeof(vis));
+    }
+
+    return 0;
+}
--- README.md
+++ README.md
@@ -1,3 +1,11 @@
-# Stack The Career 
-</br>
+# Stack The Career
+
 I hate English Problems
+
+# Build
+
+`g++ {file.cpp} -o {file}`
+
+# Run
+
+`main < input`
